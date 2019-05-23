e321b22f0d1a38ec203cab9a8674b819083d04c5
elakkiyathennarasu <50897956+elakkiyathennarasu@users.noreply.github.com>
2019-05-23 22:34:09 +0530

    Create vowel_consonant.py

---
+++ vowel_consonant.py
@@ -0,0 +1,8 @@
+a=input("")
+if(a>="a" and a<="z" or a>="A" and a<="Z"):
+  if(a=="a" or a=="e" or a=="i" or a=="o" or a=="u" or a=="A" or a=="E" or a=="I" or a=="O" or a=="U"):
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print('invalid')
